14695a351de5c2747d7871840d8a8d1334003d7f
NamDT-146 <84223717+NamDT-146@users.noreply.github.com>
2024-04-24 02:09:13 +0700

    Update scripts.js

--- innersite/scripts.js
+++ innersite/scripts.js
@@ -1,5 +1,5 @@
 var i = 0;
-var txt = 'CHÚC CHỊ TUỔI 19 VUI VẺ YÊU ĐỜI, XINH NHƯ BHX, SỐNG LÂU TRĂM TUỔI, KHÔNG ĐAU LƯNG MỎI GỐI NHƯNG CÓ NGƯỜI TRONG LÒNG ĐỂ MỎI MONG, ĐỘC TÔN TRÊN ĐƯỜNG QUAN LỘ NHƯNG ĐỘC HÀNH TRÊN ĐƯỜNG TÌNH DUYÊN';
+var txt = 'CHÚC CHỊ TUỔI 19 VUI VẺ YÊU ĐỜI, XINH NHƯ BHX, SỐNG LÂU TRĂM TUỔI, KHÔNG ĐAU LƯNG MỎI GỐI NHƯNG CÓ NGƯỜI TRONG LÒNG ĐỂ MỎI MONG, ĐỘC TÔN TRÊN ĐƯỜNG QUAN LỘ NHƯNG KHÔNG ĐỘC HÀNH TRÊN ĐƯỜNG TÌNH DUYÊN';
 var speed = 50;
 
 function typeWriter() {

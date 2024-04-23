ac102ca13a0e6c902210bac5fb8fd557be87c198
NamDT-146 <namdotuan146@gmail.com>
2024-04-23 23:51:03 +0700

    first commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+document.getElementById('buttonA').addEventListener('click', function() {
+    alert('You clicked Button A! 🌟');
+});
+
+document.getElementById('buttonB').addEventListener('click', function() {
+    alert('You clicked Button B! 🎉');
+});
